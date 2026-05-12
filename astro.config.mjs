28217a723c622ca92b435ab8d953a8305e971fac
LasperKaine <kasperlaine05@gmail.com>
2026-05-12 18:59:13 +0300

    quick fix for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.VERCEL
+  ? false
+  : process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  site: 'https://lasperkaine.github.io',
 
-  trailingSlash: 'never',
-
-  build: {
-    assets: 'assets',
-  },
+  base: isProd ? '/portfolio/' : '/',
 });
